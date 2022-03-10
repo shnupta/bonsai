a27470b96ba23f191cdd0bf9f284102090e563f3
Casey Williams <cjw19@ic.ac.uk>
2022-03-10 12:06:22 +0000

    Implement black scholes model

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # bonsai
+
+General notes for hybrid (both `__host__` and `__device__`) data structures
+is that they __can__ be dynamic in host code to allow for easy allocation and 
+initialisation. Then once in device code their sizes are fixed, but you are
+able to make modifications to the data stored. Some classes do have mixed in
+`std::*` members, I'm still unsure if this will work out or if I'll have to add
+some more of my own classes.
--- src/model/black_scholes.cuh
+++ src/model/black_scholes.cuh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "model.cuh"
+#include "../common/common.cuh"
 
 #include <cmath>
 
@@ -55,13 +56,89 @@ namespace bonsai {
         __host__
           void Allocate(const container<Time>& prdTimeline,
               const container<SampleDef>& prdDefline) override {
-            // TODO (next)
+            timeline_.clear();
+            int count = 0;
+            for (int i = 0; i < prdTimeline.size(); ++i) {
+              if (prdTimeline[i] > SYSTEM_TIME) count++;
+            }
+            timeline_.resize(count);
+            count = 0;
+            for (int i = 0; i < prdTimeline.size(); ++i) {
+              if (prdTimeline[i] > SYSTEM_TIME) {
+                timeline_[0] = prdTimeline[i];
+                count++;
+              }
+            }
+
+            isTodayOnTimeline_ = (prdTimeline[0] == SYSTEM_TIME);
+            defline_ = &prdDefline;
+
+            stds_.resize(timeline_.size() - 1);
+            drifts_.resize(timeline_.size() - 1);
+
+            const int n = prdTimeline.size();
+            numeraires_.resize(n);
+            discounts_.resize(n);
+            forwardFactors_.resize(n);
+            libors_.resize(n);
+            for (int i = 0; i < n; ++i) {
+              discounts_[i].resize(prdDefline[i].discountMats.size());
+              forwardFactors_[i].resize(prdDefline[i].forwardMats.size());
+              libors_[i].resize(prdDefline[i].liborDefs.size());
+            }
           }
 
         __host__
           void Initialise(const container<Time>& prdTimeline,
               const container<SampleDef>& prdDefline) override {
-            // TODO (next)
+            const T mu = rate_ - dividendYield_;
+            const int n = timeline_.size() - 1;
+
+            for (int i = 0; i < n; ++i) {
+              const double dt = timeline_[i+1] - timeline_[i];
+              stds_[i] = vol_ * sqrt(dt);
+
+              if (isSpotMeasure_) {
+                drifts_[i] = (mu + 0.5 * vol_ * vol_) * dt;
+              } else {
+                drifts_[i] = (mu - 0.5 * vol_ * vol_) * dt;
+              }
+            }
+
+            // Precompute the forwards, discounts and libors over the timeline
+            const int m = timeline_.size();
+            for (int i = 0; i < m; ++i) {
+              if (prdDefline[i].numeraire) {
+                if (isSpotMeasure_) {
+                  numeraires_[i] = exp(dividendYield_ * prdTimeline[i]) / spot_;
+                } else {
+                  numeraires_[i] = exp(rate_ * prdTimeline[i]);
+                }
+              }
+
+              // Forward factors
+              const int pFF = prdDefline[i].forwardMats.size();
+              for (int j = 0; j < pFF; ++j) {
+                forwardFactors_[i][j] =
+                  exp(mu * (prdDefline[i].forwardMats[j] - prdTimeline[i]));
+              }
+
+              // Discounts
+              const int pDF = prdDefline[i].discountMats.size();
+              for (int j = 0; j < pDF; ++j) {
+                discounts_[i][j] =
+                  exp(-rate_ * (prdDefline[i].discountMats[j] 
+                        - prdTimeline[i]));
+              }
+
+              // Libors
+              const int pL = prdDefline[i].liborDefs.size();
+              for (int j = 0; j < pL; ++j) {
+                const double dt = prdDefline[i].liborDefs[j].end
+                  - prdDefline[i].liborDefs[j].start;
+                libors_[i][j] = (exp(rate_ * dt) - 1.0) / dt;
+              }
+            }
           }
 
         __host__ __device__
@@ -69,10 +146,28 @@ namespace bonsai {
             return timeline_.size() - 1;
           }
 
+        // Per-thread responsability to consume the vector of gaussians 
+        // and generate a path
+        // NOTE: Given this vector of gaussians is generated from Sobol
+        // I'll need to change the ordering of the curand generated output
+        // So that each thread has a container of _timeSteps_ random variables,
+        // one from each dimension of Sobol
         __device__
           void GeneratePath(const container<double>& gaussVec,
               Scenario<T>& path) const override {
-            // TODO
+            T spot = spot_;
+            int index = 0;
+            if (isTodayOnTimeline_) {
+              FillScenario(index, spot, path[index], (*defline_)[index]);  
+              ++index;
+            }
+
+            const int n = timeline_.size() - 1;
+            for (int i = 0; i < n; ++i) {
+              spot = spot * exp(drifts_[i] + stds_[i] * gaussVec[i]);
+              FillScenario(index, spot, path[index], (*defline_)[index]);
+              ++index;
+            }
           }
 
       private:
@@ -82,6 +177,29 @@ namespace bonsai {
             parameters_[1] = &vol_;
             parameters_[2] = &rate_;
             parameters_[3] = &dividendYield_;
+          }
+
+        // Fill a sample in a Scenario given the definition and spot
+        __device__
+          inline void FillScenario(const int index, const T& spot, 
+              Sample<T>& scen, const SampleDef& def) const {
+            if (def.numeraire) {
+              scen.numeraire = numeraires_[index];
+              if (isSpotMeasure_) scen.numeraire *= spot;
+            }
+
+            // Fill forwards
+            for (int j = 0; j < forwardFactors_[index].size(); ++j) {
+              scen.forwards[j] = spot * forwardFactors_[index][j];
+            }
+
+            // Fill discounts and libors
+            for (int j = 0; j < discounts_[index].size(); ++j) {
+              scen.discounts[j] = discounts_[index][j];
+            }
+            for (int j = 0; j < libors_[index].size(); ++j) {
+              scen.libors[j] = libors_[index][j];
+            }
           }
 
 
